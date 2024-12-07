f0f36ddc1f7f81aabd363ab91f4ac992ff171c02
Rainzor <runze.wang02@outlook.com>
2024-12-07 18:06:25 +0800

    add Lab2/ddp training

--- Lab2_CNN/script/resnet18.sh
+++ Lab2_CNN/script/resnet18.sh
@@ -1,14 +1,1 @@
-torchrun --nproc_per_node=1  ./train_ddp.py \
-    -m 'resnet18' \
-    -i '/data2/wrz/Datasets/tiny-imagenet-200' \
-    -b 256 \
-    -n 50 \
-    -opt 'sgd' \
-    -lr 0.2 \
-    -o 'out' \
-    --momentum 0.9 \
-    --weight-decay 1e-4 \
-    --lr-scheduler 'cosine' \
-    --lr-warmup-epochs 5 \
-    --lr-warmup-method 'linear' \
-    --lr-warmup-decay 0.01 \
+torchrun --nproc_per_node=1  ./train_ddp.py -m 'resnet18' -i '/data2/wrz/Datasets/tiny-imagenet-200' -b 256 -n 50 -opt 'sgd' -lr 0.2 -o 'out' --momentum 0.9 --weight-decay 1e-4 --lr-scheduler 'cosine' --lr-warmup-epochs 5 --lr-warmup-method 'linear' --lr-warmup-decay 0.01
