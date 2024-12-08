47591652cb96184e7ad9b1ac7dca381e246b20b4
Rainzor <runze.wang02@outlook.com>
2024-12-08 18:08:52 +0800

    add resnext

--- Lab2_CNN/script/vgg11_ddp.sh
+++ Lab2_CNN/script/vgg11_ddp.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node=4  ./train_ddp.py -m 'vgg11' -d '/data2/wrz/Datasets/tiny-imagenet-200' -b 256 -n 100 -opt 'adam' -lr 0.1 -o 'out'  --weight-decay 1e-4 --lr-scheduler 'cosine' --lr-warmup-epochs 5 --lr-warmup-method 'linear' --lr-warmup-decay 0.01 --writer
+torchrun --nproc_per_node=4  ./train_ddp.py -m 'vgg11' -d '/data2/wrz/Datasets/tiny-imagenet-200' -b 256 -n 100 -opt 'adam' -lr 5e-4 -o 'out'  --weight-decay 1e-4 --lr-scheduler 'cosine' --lr-warmup-epochs 5 --lr-warmup-method 'linear' --lr-warmup-decay 0.01 --writer
