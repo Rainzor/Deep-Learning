8669ac91c7d4d7fbee29c07bed285b7e3c35076e
Rainzor <runze.wang02@outlook.com>
2024-12-18 19:48:03 +0800

    update

--- Lab3_RNN/scripts/test.sh
+++ Lab3_RNN/scripts/test.sh
@@ -1,33 +1,33 @@
 echo "-- Testing --"
 
-echo "Testing RNN"
+echo "1. Testing RNN"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m rnn
 
-echo "Testing LSTM"
+echo "2. Testing LSTM"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m lstm
 
-echo "Testing GRU"
+echo "3. Testing GRU"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m gru
 
-echo "Testing BiLSTM"
+echo "4. Testing BiLSTM"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m lstm --bidirectional
 
-echo "Testing RCNN"
+echo "5. Testing RCNN"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m lstm --pool max
 
-echo "Testing LSTM Attention"
+echo "6. Testing LSTM Attention"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m lstm --pool attention
 
-echo "Testing Transformer"
+echo "7. Testing Transformer"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m transformer
 
 echo "--Testing Custom--"
 
-echo "Testing Custom CNN"
+echo "8. Testing Custom CNN"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m custom_rnn
 
-echo "Testing Custom LSTM"
+echo "9. Testing Custom LSTM"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m custom_lstm
 
-echo "Testing Custom GRU"
+echo "10. Testing Custom GRU"
 python train.py -d /data2/wrz/Datasets/Yelp/ -n 1 -b 256 -lr 1e-7 -m custom_gru
