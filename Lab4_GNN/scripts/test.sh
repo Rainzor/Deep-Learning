e260493533fe4098c5797a695d6e8ff751e6d721
Rainzor <runze.wang02@outlook.com>
2025-01-01 21:39:17 +0800

    .

--- Lab4_GNN/scripts/test.sh
+++ Lab4_GNN/scripts/test.sh
@@ -3,14 +3,14 @@ root = "/data2/wrz/Datasets/"
 
 
 echo "1. Testing Cora"
-python train.py -r $root --dataset cora --tag test
+python train.py -r $root --dataset cora --tag test -n 50
 
 echo "2. Testing Citeseer"
-python train.py -r $root --dataset citeseer --tag test
+python train.py -r $root --dataset citeseer --tag test -n 50
 
 echo "3. Testing PPI"
-python train.py -r $root --dataset ppi --tag test
+python train.py -r $root --dataset ppi --tag test -n 50
 
 echo "4. Testing GAT"
-python train.py -r $root --dataset cora --tag test --model GAT
+python train.py -r $root --dataset cora --tag test --model GAT -n 50
 
