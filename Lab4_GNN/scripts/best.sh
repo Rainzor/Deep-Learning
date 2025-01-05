830cb8dfb1a3c229655bc10f757af70459351155
Rainzor <runze.wang02@outlook.com>
2025-01-05 16:04:13 +0800

    add best

--- Lab4_GNN/scripts/best.sh
+++ Lab4_GNN/scripts/best.sh
@@ -1,21 +1,21 @@
 echo "-- Testing --"
 echo "=== Node Classification ==="
 
-# Cora Node Classification: 0.810
-python train.py -r /data2/wrz/Datasets/ --dataset cora --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation gelu --tag best
+# Cora Node Classification: 0.806
+python train.py -r /data2/wrz/Datasets/ --dataset cora --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation relu --tag relu
 
-# Citeseer Node Classification: 0.7230
-python train.py -r /data2/wrz/Datasets/ --dataset citeseer --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation gelu --tag best
+# Citeseer Node Classification: 0.718
+python train.py -r /data2/wrz/Datasets/ --dataset citeseer --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation relu --tag best
 
-# PPi Node Classification: 0.7464
-python train.py -r /data2/wrz/Datasets/ --dataset ppi --task node-cls -lr 0.02 -n 200 --patience 100 --hidden-dim 256 -nl 4 -pn PN-SI --edge-drop 0.5 --tag best
+# PPi Node Classification: 0.7465
+python train.py -r /data2/wrz/Datasets/ --dataset ppi --task node-cls -lr 0.02 -n 200 --patience 100 --hidden-dim 256 -nl 4 -pn PN-SI --activation relu --tag best
 
 echo "=== Link Prediction ==="
 # Cora Link Prediction: 0.7562
-python train.py -r /data2/wrz/Datasets/  --dataset cora --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 2 --activation gelu --edge-drop 0.5 -pn PN-SI --tag best
+python train.py -r /data2/wrz/Datasets/ --dataset cora --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 4  --edge-drop 0.1 --activation relu --tag best
 
 # Citeseer Link Prediction: 0.7681
-python train.py -r /data2/wrz/Datasets/  --dataset citeseer --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 4 -pn PN-SI --edge-drop 0.5 --tag best
+python train.py -r /data2/wrz/Datasets/ --dataset citeseer --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 4 -pn PN-SI --activation relu --tag best
 
 # PPI Link Prediction:  0.6882
-python train.py -r /data2/wrz/Datasets/  --dataset ppi --task link-pred -lr 0.02 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 256 -nl 4 -pn PN-SI --edge-drop 0.5 --tag best
+python train.py -r /data2/wrz/Datasets/ --dataset ppi --task link-pred -lr 0.02 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 256 -nl 4  --edge-drop 0.1 --activation relu --tag best
--- Lab4_GNN/scripts/cmp_relu.sh
+++ Lab4_GNN/scripts/cmp_relu.sh
@@ -14,21 +14,15 @@ done
 
 echo "=== Node Classification ==="
 
-# Cora Node Classification: 0.810
 python train.py -r /data2/wrz/Datasets/ --dataset cora --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation relu --tag relu
 
-# Citeseer Node Classification: 0.7230
 python train.py -r /data2/wrz/Datasets/ --dataset citeseer --task node-cls -lr 0.01 -n 200 --patience 50 --hidden-dim 128 -nl 2 --activation relu --tag relu
 
-# PPi Node Classification: 0.7438
 python train.py -r /data2/wrz/Datasets/ --dataset ppi --task node-cls -lr 0.02 -n 200 --patience 100 --hidden-dim 256 -nl 2 --activation relu --tag relu
 
 echo "=== Link Prediction ==="
-# Cora Link Prediction: 0.6973
 python train.py -r $root --dataset cora --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 2 --activation relu --tag relu
 
-# Citeseer Link Prediction: 0.7077
 python train.py -r $root --dataset citeseer --task link-pred -lr 0.01 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 128 -nl 2 --activation relu --tag relu
 
-# PPI Link Prediction:  0.6598
 python train.py -r $root --dataset ppi --task link-pred -lr 0.02 -n 200 -ws 10 --patience -1 --scheduler cosine --hidden-dim 256 -nl 2 --activation relu --tag relu
